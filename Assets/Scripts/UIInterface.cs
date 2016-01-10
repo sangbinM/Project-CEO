5fbaec058be412b4d9e68af7e7e1af73562b6c95
sejinvv <sksl_93@naver.com>
2016-01-10 14:32:15 +0900

    Change

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIInterface : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Text TimeText;
+    public Text Name;
+    public Image DistanceBar;
+    
+
+    public void SkillBtCheck()  //  어택 버튼 눌렸을 때 스킬까지 사용하는 지 아닌지 체크
+    {
+
+
+    }
+
+    public void AttackBt()  // 어택 버튼 눌렸을 때 실행
+    {
+
+    }
+
+    public void JumpBt()    // 점프  버튼 눌렸을 떄 실행
+    {
+
+
+    }
+
+
 }
