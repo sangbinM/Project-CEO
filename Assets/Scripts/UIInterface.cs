a6a65b5e4413df5b5f1b5621d40eef28fd0dca0e
sejinvv <sksl_93@naver.com>
2016-01-10 14:38:03 +0900

    Revert "hi"
    This reverts commit b6971e30c986fd10da9c70074cc625fb0a487c7a.

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -1,30 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class UIInterface : MonoBehaviour {
 
-    public Text TimeText;
-    public Text Name;
-    public Image DistanceBar;
-    
-
-    public void SkillBtCheck()  //  어택 버튼 눌렸을 때 스킬까지 사용하는 지 아닌지 체크
-    {
-
-
-    }
-
-    public void AttackBt()  // 어택 버튼 눌렸을 때 실행
-    {
-
-    }
-
-    public void JumpBt()    // 점프  버튼 눌렸을 떄 실행
-    {
-
-
-    }
-
-
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
