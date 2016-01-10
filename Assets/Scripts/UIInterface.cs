1bfff7261d592a964fdc12d21854a0c83598eb6c
sejinvv <sksl_93@naver.com>
2016-01-10 17:02:29 +0900

    플레이 화면 UIinterface완성
    스킬 버튼 드래그 클릭 완성

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -15,8 +15,11 @@ public class UIInterface : MonoBehaviour
 
     private int _layerMask;
 
+    private int flag;
+
     void Awake() {
 
+        flag = 0;
         TimeText.text = "00:01";
         PlayerName.text = "대리";
         CompanyName.text = "NCSOFT";
@@ -26,29 +29,48 @@ public class UIInterface : MonoBehaviour
 
     }
 
-    void Update()
-    {
 
+    void Update() {
 
-        if (Input.GetMouseButtonDown(0))
-        {
+        ButtonCheck();
 
-            if (CheckTouchUI(Input.mousePosition, AttackButton))
-            {
-                AttackBt();
+    }
 
+    private void ButtonCheck() {
+
+        // flag 는 공격 버튼 눌렀을 때 활성화
+
+        if (flag == 1 && Input.GetMouseButton(0)) {
+            if (CheckTouchUI(Input.mousePosition, SkillButton)) { // 드래그 한 것이 스킬 버튼까지 오면
+                SkillBt();
+                flag = 0;
             }
-            else if (CheckTouchUI(Input.mousePosition, JumpButton))
+
+        } else if (Input.GetMouseButtonUp(0) && flag == 1)    //  공격 버튼 누르고 떼면
+        {
+            if (CheckTouchUI(Input.mousePosition, AttackButton)) 
             {
+                flag = 1;
+                AttackBt();
+            }
+        } else if (Input.GetMouseButtonDown(0)) {
+
+            if (CheckTouchUI(Input.mousePosition, AttackButton)){
+                flag = 1;
+
+            } else if (CheckTouchUI(Input.mousePosition, JumpButton)){
 
                 JumpBt();
             }
 
+        }else {
 
-
+            flag = 0;
         }
+
     }
-    private bool CheckTouchUI(Vector3 position, Image img) {
+
+    private bool CheckTouchUI(Vector3 position, Image img) {        //버튼이 사각형 일 때 
 
         if (img.transform.position.x + img.rectTransform.rect.width / 2 > position.x
             && img.transform.position.x - img.rectTransform.rect.width / 2 < position.x
@@ -62,38 +84,32 @@ public class UIInterface : MonoBehaviour
         return false;
     }
 
-  
-    private bool SkillBtCheck()  //  어택 버튼 눌렸을 때 스킬까지 사용하는 지 아닌지 체크
+    private bool CheckTouchUIRound(Vector3 position, Image img) // 버튼이 원일 때
     {
-        while (Input.GetMouseButton(0)) // 마우스가 계속 눌리는 동안 
-        {
-            if (CheckTouchUI(Input.mousePosition, SkillButton)) {
-                return true;
-            }
-            
+        float r = img.rectTransform.rect.width / 2;
+        if ((img.transform.position.x - position.x) * (img.transform.position.x - position.x) 
+            + (img.transform.position.y - position.y) * (img.transform.position.y - position.y) < r*r){
+
+            return true;
+
         }
 
         return false;
+    }
 
+
+    public void SkillBt()  // 어택 버튼 눌렸을 때 실행
+    {
+
+        Debug.Log("스킬 시전");
+        
     }
 
     public void AttackBt()  // 어택 버튼 눌렸을 때 실행
     {
-        bool value = SkillBtCheck();
-        
 
-        if (value)
-        {
-            //skill  실전
-            Debug.Log("스킬 시전");
-
-
-        }
-        else {
-
-            Debug.Log("공격 시전");
-        }
-
+       Debug.Log("공격 시전");
+       
     }
 
     public void JumpBt()    // 점프  버튼 눌렸을 떄 실행
