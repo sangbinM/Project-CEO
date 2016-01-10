185d5f9b8ad3a28cd28ea5891e1f959e71ff96eb
sangbinM <sm3909@columbia.edu>
2016-01-10 18:05:53 +0900

    엔딩 크레딧 생성

---
+++ Assets/Scripts/EndingCredit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingCredit : MonoBehaviour {
+
+    private float moveSpeed = 50.0f;
+
+    public new Transform transform;
+
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = new Vector3(0f, moveSpeed, 0f) * Time.deltaTime;
+        Debug.Log(movement.y);
+        transform.Translate(movement);
+	}
+}
