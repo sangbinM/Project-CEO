191c444bfdbd5c8e283c6afe9aa224faf859764a
sangbinM <sm3909@columbia.edu>
2016-01-10 16:59:06 +0900

    캐릭터 점프 구현
    FSMBase만듦: PlayerController와 ObstacleController가 상속
    PlayerController 만듦

---
+++ Assets/Scripts/FSMBase.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FSMBase : MonoBehaviour {
+
+    public enum State
+    {
+        Run, Attack, Jump, Dead
+    }
+
+    public State state = State.Run;
+
+    [HideInInspector]
+    public new Transform transform;
+
+    protected CharacterController _cc;
+    protected Animator _animator;
+
+    protected virtual void Awake()
+    {
+        transform = GetComponent<Transform>();
+        _cc = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    protected virtual void Update()
+    {
+        Invoke(state.ToString(), 0f);
+    }
+
+    public void SetState(State newState)
+    {
+        state = newState;
+        _animator.SetInteger("state", (int)state);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : FSMBase {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private string playerName;
+
+    private float characterAltitude;
+    private float jumpSpeed;
+    private float maxSpeed = 20f;
+    private float characterMass = 10f;
+
+
+    private int _layermask;
+
+    private ObstacleController _obstacle;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        characterAltitude = transform.position.y;
+        SetName("Team9");
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (state == State.Dead)
+            return;
+
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (state == State.Run)
+            {
+                jumpSpeed = maxSpeed;
+                SetState(State.Jump);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SetState(State.Attack);
+        }
+    }
+
+    protected virtual void Run()
+    {
+
+    }
+
+    protected virtual void Attack()
+    {
+        if (Vector3.Distance(transform.position, _obstacle.transform.position) <= 2f)
+        {
+            //_obstacle.Damage();
+        }
+
+        SetState(State.Run);
+    }
+
+    protected virtual void Jump()
+    {
+        jumpSpeed += characterMass * Physics.gravity.y * Time.deltaTime * 1/2;
+        Vector3 movement = new Vector3(0f, jumpSpeed * Time.deltaTime);
+        Debug.Log(jumpSpeed * Time.deltaTime);
+        _cc.Move(movement);
+
+        if (transform.position.y <= characterAltitude)
+        {
+            transform.position = new Vector3(transform.position.x, characterAltitude);
+            SetState(State.Run);
+        }
+    }
+
+    protected virtual void Dead()
+    {
+
+    }
+
+    public void SetName(string name)
+    {
+        playerName = name;
+    }
+
+    public string GetName()
+    {
+        return playerName;
+    }
 }
