cb8f25527911b9865a03ab6efb1c78255a1ee355
sejinvv <sksl_93@naver.com>
2016-01-10 20:53:30 +0900

    플레이 화면 - 배경 움직임

--- Assets/Scripts/BackGroundMove.cs
+++ Assets/Scripts/BackGroundMove.cs
@@ -3,24 +3,43 @@ using System.Collections;
 
 public class BackGroundMove : MonoBehaviour {
 
-    //private float moveSpeed = 50.0f;
-
-    public new Transform transform;
+    private float moveSpeed = 5.0f;
+    
     private Transform[] background;
-
+    private int flag;   // 화면 체인지 해줘야 하는 값
+    public float xSize; // Scene에서 Setting 해줘야 하는 값
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
-        transform = GetComponent<Transform>();
-       // background = GameObject.FindGameObjectsWithTag("Background")
+        flag = 0;
+        
+        GameObject[] gameobjects = GameObject.FindGameObjectsWithTag("Background");
+
+        background = new Transform[gameobjects.Length];
+        
+
+        background[0] = gameobjects[0].GetComponent<Transform>();
+        background[1] = gameobjects[1].GetComponent<Transform>();
+
+
     }
     // Update is called once per frame
     void Update () {
         
+        
+        Vector3 movement = new Vector3(-moveSpeed, 0f, 0f) * Time.deltaTime;
+        background[0].Translate(movement);
+        background[1].Translate(movement);
 
-        //Vector3 movement = new Vector3(0f, moveSpeed, 0f) * Time.deltaTime;
-        //Debug.Log(movement.y);
-        //transform.Translate(movement);
+        if (background[0].position.x <= 0 && flag == 0) {
+            background[1].position = new Vector3(xSize, 0, background[1].position.z);
+            flag = 1;
+        } else if (background[1].position.x <= 0 && flag == 1){
+            background[0].position = new Vector3(xSize, 0, background[0].position.z);
+            flag = 0;
+        }
+
+
     }
 }
