698fb93f777eae6a7b90fd3ac14cf6896c147ad4
sejinvv <sksl_93@naver.com>
2016-01-10 16:38:03 +0900

    UI interface 수정 - 버튼 터치 수정중
    그 외 UI 포맷 만들어놈

--- Assets/Scripts/UIInterface.cs
+++ Assets/Scripts/UIInterface.cs
@@ -6,30 +6,71 @@ public class UIInterface : MonoBehaviour
 {
 
     public Text TimeText;
-    public Text Name;
+    public Text PlayerName;
+    public Text CompanyName;
     public Image DistanceBar;
+    public Image JumpButton;
+    public Image AttackButton;
+    public Image SkillButton;
+
+    private int _layerMask;
+
+    void Awake() {
+
+        TimeText.text = "00:01";
+        PlayerName.text = "대리";
+        CompanyName.text = "NCSOFT";
+        DistanceBar.fillAmount = 0.5f;
+        print("button");
 
 
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (CheckTouchUI(Input.mousePosition, AttackButton))
+            {
+                AttackBt();
+
+            }
+            else if (CheckTouchUI(Input.mousePosition, JumpButton))
+            {
+
+                JumpBt();
+            }
+
+
+
+        }
+    }
+    private bool CheckTouchUI(Vector3 position, Image img) {
+
+        if (img.transform.position.x + img.rectTransform.rect.width / 2 > position.x
+            && img.transform.position.x - img.rectTransform.rect.width / 2 < position.x
+            && img.transform.position.y + img.rectTransform.rect.height / 2 > position.y
+            && img.transform.position.y - img.rectTransform.rect.height / 2 < position.y) {
+
+            return true;
+
+        }
+
+        return false;
+    }
+
+  
     private bool SkillBtCheck()  //  어택 버튼 눌렸을 때 스킬까지 사용하는 지 아닌지 체크
     {
         while (Input.GetMouseButton(0)) // 마우스가 계속 눌리는 동안 
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            print("pressed");
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 100f))
-            {
-
-                string tag = hit.transform.gameObject.tag;
-                
-                if (tag == "SkillButton")
-                {
-
-                    return true;
-
-                }
+            if (CheckTouchUI(Input.mousePosition, SkillButton)) {
+                return true;
             }
+            
         }
 
         return false;
@@ -61,4 +102,7 @@ public class UIInterface : MonoBehaviour
         Debug.Log("점프 시전");
 
     }
+
+    
+
 }
