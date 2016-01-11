368953413872b6843647f2a73eb538c3a14b118c
jhl07 <hljoo0887@gmail.com>
2016-01-11 22:10:49 +0900

    obstace중 11

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ObstacleController : MonoBehaviour {
+public class ObstacleController : FSMBase {
+    
+    public enum ObState
+    {
+        Run, Dead
+    }
+    
+    public float moveSpeed = 2f;
+    public float turnSpeed = 360f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float attack = 5f;
+    public float attackRange = 2f;
+    public int currentHP = 30;
+
+    private int _maxHP;
+
+    public float waitTime = 1f;
+
+    public float deadTime = 3f;
+
+    public Transform waypointRoot;
+
+    //protected Transform[] _waypoints;
+    protected Camera _sight;
+    protected PlayerController _player;
+
+    private float _elapsedTime = 0f;
+    private Transform _currentWayPoint;
+
+    //    void Awake()
+    //    {
+    //        _obstacle = GetComponent<ObstacleController>();
+    //    }
 }
