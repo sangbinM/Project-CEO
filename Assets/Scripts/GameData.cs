821db2315f2c37d9d471d55e9daf582348a1b3fc
sangbinM <sm3909@columbia.edu>
2016-01-12 22:47:39 +0900

    플레이 데이터 .dat 형식으로 저장/불러오기 및 게임 내에서 데이터 유지

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameData : MonoBehaviour {
+
+    public static GameData data;
+
+    public string playerName;
+    public int[] stars;
+    public int[] times;
+
+    void Awake()
+    {
+        if (data == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            data = this;
+        }
+        else if (data != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+        PlayerData data = new PlayerData();
+        data.playerName = playerName;
+        data.stars = stars;
+        data.times = times;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            playerName = data.playerName;
+            stars = data.stars;
+            times = data.times;
+        }
+    }
+
+    //GameData.data.stars[1]
+    //GameData.data.times[2]
+    //GameData.data.Save();
+}
+
+// serializable class -> can save to a file
+[Serializable]
+class PlayerData
+{
+    public string playerName;
+    public int[] stars;
+    public int[] times;
+}
