3a8a958b8f014750eeb81a4777ff50f350ae9e22
sejinvv <sksl_93@naver.com>
2016-01-17 01:31:59 +0900

    설명 씬 추가

---
+++ Assets/Scripts/Description.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Description : MonoBehaviour {
+
+    public Text[] descriptionText = new Text[3];
+    public string sceneToLoad = "Map";
+
+    // Use this for initialization
+    void Start () {
+        descriptionText[1].enabled = false;
+        descriptionText[2].enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void NextPage()
+    {
+        if (descriptionText[0].enabled == true)
+        {
+            descriptionText[0].enabled = false;
+            descriptionText[1].enabled = true;
+        }
+        else if (descriptionText[1].enabled == true)
+        {
+            descriptionText[1].enabled = false;
+            descriptionText[2].enabled = true;
+        }
+        else if (descriptionText[2].enabled == true)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
